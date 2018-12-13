4b04699194ac5ec668d330c6b1ebd516dfc9bc92
bgs-blue <william.stubler@gmail.com>
2018-12-13 12:51:27 -0700

    tmux uses fish, bring back fish pager

--- files-to-symlink/.config/fish/config.fish
+++ files-to-symlink/.config/fish/config.fish
@@ -1,6 +1,5 @@
 function fish_user_key_bindings
          fish_default_key_bindings
-         bind \ep history-search-backward
          bind \em beginning-of-line
          bind \eK kill-whole-line
 end
