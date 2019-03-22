afa8d818949d627f8b90b1d113db4999019a5e3c
bgs-blue <william.stubler@gmail.com>
2019-03-22 10:10:08 -0700

    removed dotfiles

--- dotfiles/.config/fish/functions/fish_user_key_bindings.fish
+++
@@ -1,7 +0,0 @@
-# Defined in /home/wgs/.config/fish/config.fish @ line 1
-function fish_user_key_bindings
-    fish_default_key_bindings
-    bind \em beginning-of-line
-    bind \eK kill-whole-line
-    bind \ek backward-kill-word
-end
